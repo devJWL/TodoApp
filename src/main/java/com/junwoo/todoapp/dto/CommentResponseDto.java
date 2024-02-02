51fb00823c97c62ae13621114aa170d8986179e8
devJWL <jwlee4450@gmail.com>
2024-02-02 10:44:18 +0900

    @JsonInclude
    @JsonInclude(JsonInclude.Include.NON_NULL)
    추가하여 NULL필드 반환에서 제외

--- src/main/java/com/junwoo/todoapp/dto/CommentResponseDto.java
+++ src/main/java/com/junwoo/todoapp/dto/CommentResponseDto.java
@@ -1,12 +1,14 @@
 package com.junwoo.todoapp.dto;
 
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.junwoo.todoapp.entity.Comment;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class CommentResponseDto {
   private String commentContents;
   private String message;
--- src/main/java/com/junwoo/todoapp/dto/TodoResponseDto.java
+++ src/main/java/com/junwoo/todoapp/dto/TodoResponseDto.java
@@ -1,11 +1,13 @@
 package com.junwoo.todoapp.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.junwoo.todoapp.entity.Todo;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class TodoResponseDto {
   private String todoTitle;
   private String todoContents;
