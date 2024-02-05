96b4707149aebfb9c028f1ddedc6b9750bee594e
devJWL <jwlee4450@gmail.com>
2024-02-06 08:56:28 +0900

    Query 가독성

--- src/main/java/com/junwoo/todoapp/repository/TodoRepository.java
+++ src/main/java/com/junwoo/todoapp/repository/TodoRepository.java
@@ -16,7 +16,8 @@ public interface TodoRepository extends JpaRepository<Todo, Long> {
 
   @Query("""
       select t from Todo t
-      where t.user.userId = ?1 and t.todoTitle like concat('%', ?2, '%') or t.hidden = false and t.todoTitle like concat('%', ?2, '%')""")
+      where (t.user.userId = ?1 and t.todoTitle like concat('%', ?2, '%')) 
+      or (t.hidden = false and t.todoTitle like concat('%', ?2, '%'))""")
   List<Todo> getAllTodoByTitle(
       Long user_userId, String todoTitle
   );
